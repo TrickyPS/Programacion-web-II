f4422cbee6dd4ef3434b34f3fef4cb4d4de116c1
David Montemayor <david.montemayor@netpay.com.mx>
2022-02-26 10:19:29 -0600

    add un poco a controllers

--- backend/src/controllers/notifications.controller.js
+++ backend/src/controllers/notifications.controller.js
@@ -2,7 +2,10 @@ const notificacionModel = require("./../models/notifications.model");
 
 exports.getUserNotifications = async(req, res) => {
     try {
-        
+        const {id} = req.params;
+        const notifications = await notificacionModel.findById(id);
+        if(!notifications) res.send({message: "No hay notificaciones"}).end();
+        res.send({data: notifications});
     } catch (error) {
         console.log(error);
         res.send({message: error});
